4639ec7ce3c5c9b752a9464ba2445d33f7f12a5c
Oscar Olivares <oscarolivares625@gmail.com>
2019-07-19 23:05:15 -0400

    Se agrega la funcionalidad para listar los usuarios

--- src/app/routes/home.js
+++ src/app/routes/home.js
@@ -1,5 +1,16 @@
+import db from '../../db/models/index';
+
 export default app => {
   app.get('/', (req, res) => {
-    res.render('home/home');
+    /* res.render('home/home'); */
+    /* db.User.findAll().then(users => {
+      const id = users[0].id;
+      res.json(users);
+    }); */
+    db.User.findAll({ raw: true }).then(users => {
+      res.render('home/home', {
+        users
+      });
+    });
   });
 };
