666eec716e0160e6ca0719458616a20e44b25eea
Oscar Olivares <oscarolivares625@gmail.com>
2019-07-20 14:09:12 -0400

    Se agregan manejadores de errores
    1) 404 y 500 para express por medio de middlewares.
    2) 500 para la coneccion a la db dentro de los enrrutadores
    
    El punto 1 tiene el objetivo de enviar una vista amigrable al usuario durante el error (falta crear la vista).
    El punto 2 corrige el problema de la espera infinita del navegador

--- src/app/routes/home.js
+++ src/app/routes/home.js
@@ -2,26 +2,38 @@ import db from '../../db/models/index';
 
 export default app => {
   app.get('/users', (req, res) => {
+    //Find all elements from db and send them to the view
+
     /* res.render('home/home'); */
     /* db.User.findAll().then(users => {
       const id = users[0].id;
       res.json(users);
     }); */
-    db.User.findAll({ raw: true }).then(users => {
-      res.render('home/home', {
-        users
+    db.User.findAll({ raw: true })
+      .then(users => {
+        res.render('home/home', {
+          users
+        });
+      })
+      .catch(err => {
+        res.status(500).send('Connection refused');
       });
-    });
   });
 
   app.post('/users/add', (req, res) => {
-    db.User.create(req.body).then(result => {
-      console.log(
-        result.get({
-          plain: true
-        })
-      );
-      res.redirect('/users');
-    });
+    // Create an user
+
+    db.User.create(req.body)
+      .then(result => {
+        console.log(
+          result.get({
+            plain: true
+          })
+        );
+        res.redirect('/users');
+      })
+      .catch(err => {
+        res.status(500).send('Connection refused');
+      });
   });
 };
