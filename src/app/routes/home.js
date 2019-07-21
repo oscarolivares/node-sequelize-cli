1d92cef0ad905d19eb45974bad8cb8087bb78281
Oscar Olivares <oscarolivares625@gmail.com>
2019-07-21 00:53:18 -0400

    Ahora la app cuenta con todos los metodos del CRUD

--- src/app/routes/home.js
+++ src/app/routes/home.js
@@ -5,26 +5,21 @@ export default app => {
     // Find all elements from db and send them to the view
     // This route also get data from user/add redirect
 
-    /* res.render('home/home'); */
-    /* db.User.findAll().then(users => {
-      const id = users[0].id;
-      res.json(users);
-    }); */
-
     const addStatus = req.query.addStatus;
 
     db.User.findAll({ raw: true })
       .then(users => {
         res.render('home/home', {
           users,
-          addStatus
+          addStatus,
+          error: null
         });
       })
       .catch(err => {
         res.status(500).render('home/home', {
           users: null,
           addStatus,
-          error: 'db-connection-refused',
+          error: 'db-connection-refused'
         });
       });
   });
@@ -34,20 +29,50 @@ export default app => {
 
     db.User.create(req.body)
       .then(result => {
-        console.log(
+        /* console.log(
           result.get({
             plain: true
           })
-        );
+        ); */
         res.redirect('/users');
       })
       .catch(err => {
-        /* res.status(500).render('home/home', {
-          users: null,
-          error: 'connection refused'
-        }); */
         const addStatus = encodeURIComponent('fail');
         res.redirect('/users/?addStatus=' + addStatus);
+      });
+  });
+
+  app.post('/users/delete', (req, res) => {
+    // Delete an user by id
+
+    db.User.destroy({
+      where: {
+        id: req.body.id
+      }
+    })
+      .then(result => {
+        res.redirect('/users');
+      })
+      .catch(err => {
+        const delStatus = encodeURIComponent('fail');
+        res.redirect('/users/?delStatus=' + delStatus);
+      });
+  });
+
+  app.post('/users/update', (req, res) => {
+    // Update an user by id
+    
+    db.User.update(req.body, {
+      where: {
+        id: req.body.id
+      }
+    })
+      .then(result => {
+        res.redirect('/users');
+      })
+      .catch(err => {
+        const updateStatus = encodeURIComponent('fail');
+        res.redirect('/users/?updateStatus=' + updateStatus);
       });
   });
 };
